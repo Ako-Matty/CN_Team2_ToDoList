310fe1a3cc731d38470af1a04d88d840d0c8c3f7
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-15 12:18:40 +0000

    added functions for user login and routes

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -2,8 +2,34 @@ const User = require("./model");
 const jwt = require("jsonwebtoken")
 
 
+const login = async (req, res) => {
+    try {
+        if (req.authCheck) {
+            res.status(201).json({
+                message: "success",
+                user: {
+                    user: {
+                        username: req.authCheck.username
+                    }
+                }
+            })
+            return;
+        };
+        
+        const token = await jwt.sign({ id: req.user.id }, process.env.SECRET);
 
+        res.status(201).json({message: "success",
+    user: {
+        username: req.user.username,
+        token: token
+    }
+});
+    } catch (error) {
+        res.status(501).json({ errormessage: error.message, error: error });
+    }
+};
 
 module.exports = {
-    
+    login,
+
 };
