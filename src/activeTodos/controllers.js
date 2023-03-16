e66f41ddb05d3c4d8e592bbe4ab560c2a43f1896
Matthew Halton <matthewhalton@hotmail.co.uk>
2023-03-16 12:12:16 +0000

    added activeToDo functions and routes

--- src/activeTodos/controllers.js
+++ src/activeTodos/controllers.js
@@ -1,4 +1,5 @@
 const ActiveTodo = require("./model")
+const User = require("../user/model")
 
 
 
@@ -6,6 +7,7 @@ const addActiveTodo = async (req, res) => {
     try {
         if (req.authCheck) {
             const newActiveTodo = await ActiveTodo.create(req.body)
+
             res.status(201). json({
 
                 message: "success",
@@ -18,6 +20,44 @@ const addActiveTodo = async (req, res) => {
 };
 
 
+
+
+
+
+const deleteActiveToDo = async (req, res) => {
+    try {
+        if(req.authCheck) {
+            const user = await user.findone({
+                where: {
+                    todo: req.authCheck.username
+                }
+            });
+
+            const removeActiveTodo = await ActiveTodo.destroy({
+                where: {
+                    todo: req.body.todo
+                }
+            });
+            res.status(204).json({
+                message: "success",
+                todo: removeActiveTodo
+            })
+        }
+        if(!req.body.todo){
+            const error = new Error("todo not found");
+            req.status(404).json({ message: "error", error: Error})
+        }  
+    } catch (error) {
+        res.status(501).json({ message })
+        
+    }
+};
+
+
+
+
 module.exports = {
-addActiveTodo
+addActiveTodo,
+deleteActiveToDo
+
 }
--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,4 +1,5 @@
 const User = require("./model");
+
 const jwt = require("jsonwebtoken")
 
 
